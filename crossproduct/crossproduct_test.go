866c79cc70a64a45d0df46bcba9ac40e529871e2
Zachary Taylor <zaataylor@github.com>
2022-10-23 20:19:02 -0400

    Add tests using stretch/testify! 😄

--- crossproduct/crossproduct_test.go
+++ crossproduct/crossproduct_test.go
@@ -1,8 +1,9 @@
 package crossproduct
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type Job struct {
@@ -11,37 +12,77 @@ type Job struct {
 }
 
 func TestCorrectCrossProduct(t *testing.T) {
-	sliceA := []int{1, 8}
-	sliceB := []bool{true, false}
-	sliceC := []int{15, 17, 18}
-	sliceD := []string{"testing", "more", "things"}
-	sliceE := []float64{1.7, 1.77, 1.776, 1.7769}
-	sliceJ := []Job{
-		{
-			name: "test job",
-			id:   1,
-		},
-		{
-			name: "another test job",
-			id:   2,
-		},
+	sliceInts := []int{1, 8}
+	sliceBools := []bool{true, false}
+	sliceStrings := []string{"testing", "more", "things"}
+	job1 := Job{
+		name: "test job",
+		id:   1,
+	}
+	job2 := Job{
+		name: "another test job",
+		id:   2,
+	}
+	sliceJobs := []Job{
+		job1,
+		job2,
 	}
 
-	slices := []interface{}{sliceA, sliceB, sliceC, sliceD, sliceE}
 	// Construct Cross Product from these slices
-	cp := NewCrossProduct(slices)
-	fmt.Printf("Value of cross products themselves is:\n%s", cp)
-
-	cp.printIndicesOnly = true
-	fmt.Printf("Value of cross product indices is: \n%s", cp)
+	intsAndBools := []any{sliceInts, sliceBools}
+	cp := NewCrossProduct(intsAndBools)
+	actual := cp.Values()
+	expected := []any{
+		[]any{1, true},
+		[]any{1, false},
+		[]any{8, true},
+		[]any{8, false},
+	}
+	assert.Equal(t, expected, actual)
 
 	// Construct Cross Product from these other slices
-	otherSlices := []interface{}{sliceA, sliceJ}
-	anotherCP := NewCrossProduct(otherSlices)
-	fmt.Printf("Cross Product of otherSlices: %s", anotherCP)
+	intsAndJobs := []any{sliceInts, sliceJobs}
+	anotherCP := NewCrossProduct(intsAndJobs)
+	actual = anotherCP.Values()
+	expected = []any{
+		[]any{1, job1},
+		[]any{1, job2},
+		[]any{8, job1},
+		[]any{8, job2},
+	}
+	assert.Equal(t, expected, actual)
 
-	// Construct another Cross Product from yet other slices
-	moreSlices := []interface{}{sliceA, sliceB}
-	yetAnotherCP := NewCrossProduct(moreSlices)
-	fmt.Printf("Cross Product of moreSlices: %s", yetAnotherCP)
+	// Construct another Cross Product
+	boolsAndStrings := []any{sliceBools, sliceStrings}
+	yetAnotherCP := NewCrossProduct(boolsAndStrings)
+	actual = yetAnotherCP.Values()
+	expected = []any{
+		[]any{true, "testing"},
+		[]any{true, "more"},
+		[]any{true, "things"},
+		[]any{false, "testing"},
+		[]any{false, "more"},
+		[]any{false, "things"},
+	}
+	assert.Equal(t, expected, actual)
+
+	// Construct another one!
+	jobsAndStringsAndInts := []any{sliceJobs, sliceStrings, sliceInts}
+	oneMoreCP := NewCrossProduct(jobsAndStringsAndInts)
+	actual = oneMoreCP.Values()
+	expected = []any{
+		[]any{job1, "testing", 1},
+		[]any{job1, "testing", 8},
+		[]any{job1, "more", 1},
+		[]any{job1, "more", 8},
+		[]any{job1, "things", 1},
+		[]any{job1, "things", 8},
+		[]any{job2, "testing", 1},
+		[]any{job2, "testing", 8},
+		[]any{job2, "more", 1},
+		[]any{job2, "more", 8},
+		[]any{job2, "things", 1},
+		[]any{job2, "things", 8},
+	}
+	assert.Equal(t, expected, actual)
 }
